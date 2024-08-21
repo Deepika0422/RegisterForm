a094af9581300a9d309ff8a7ea4b6797ec8a58ed
Deepika0422 <deepikaperumal0405@gmail.com>
2024-08-22 01:04:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+document.getElementById('form').addEventListener('submit', function(event) {
+    var username = document.getElementById('name').value;
+    var password = document.getElementById('password').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    const male_id=document.getElementById('male');
+    const female_id=document.getElementById('female');
+
+    
+    if (username === '' || password === '' || email === '' || phone === '' ) {
+        alert('Please fill in all fields.');
+        event.preventDefault(); 
+        return;
+    }
+
+    if (password.length < 8) {
+        alert('Password must be at least 8 characters long.');
+        event.preventDefault();
+        return;
+    }
+
+    
+    if (!email.includes('@' && '.com')) {
+        alert('Please enter a valid email address.');
+        event.preventDefault();
+        return;
+    }
+
+    
+    if (phone.length < 10 || isNaN(phone)) {
+        alert('Phone number must be at least 10 digits long and contain only numbers.');
+        event.preventDefault();
+        return;
+    }
+    if (!male_id.checked && !female_id.checked) {
+        alert('Please select your gender.');
+        event.preventDefault(); 
+    }
+});
